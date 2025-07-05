e2a94b5e580ea87265f997362fe5638087076a73
Ammar Ahmed <ammar7298@gmail.com>
2025-07-05 07:26:32 -0700

    changse

--- app/page.jsx
+++ app/page.jsx
@@ -20,20 +20,8 @@ export default function Home() {
       <Navbar />
       <main>
         {/* Full-page animated background */}
-        <div className="fixed inset-0 w-full h-full z-[-1] pointer-events-none">
-          <Waves
-            lineColor="#1E90FF"
-            backgroundColor="rgba(173, 216, 230, 0.2)"
-            waveSpeedX={0.02}
-            waveSpeedY={0.01}
-            waveAmpX={40}
-            waveAmpY={20}
-            friction={0.9}
-            tension={0.01}
-            maxCursorMove={120}
-            xGap={12}
-            yGap={36}
-          />
+        <div className="fixed bg-black inset-0 w-full h-full z-[-1] pointer-events-none">
+        
         </div>
         <Hero />
         <Skills />
